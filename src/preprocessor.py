4cfb4d40478a2e52fca1bc0a60142719dd9186c9
Chizoba Obasi <CEO@Chizobas-MacBook-Pro.local>
2018-11-21 03:10:36 -0600

    random forest classifier, accuracy, f1, recall, precision, roc curve, roc_auc_score

---
+++ src/preprocessor.py
@@ -0,0 +1,8 @@
+def preprocessor(csv_file):
+    '''
+    :param csv_file: [type: csv file]
+    :return: df: [type: pandas dataframe]
+
+    A function that takes in a csv_file and reads in the file into a pandas dataframe
+    then pre-processes the file via feature engineering for modeling readiness
+    '''
