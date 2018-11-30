4396a9af7b5f7aabde8be0edc029d98a84d6fbd4
Chizoba Obasi <CEO@Chizobas-MacBook-Pro.local>
2018-11-30 16:43:09 -0600

    clustering percentiles

--- src/clustering.py
+++ src/clustering.py
@@ -5,6 +5,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler, MinMaxScaler, normalize
 
 
 def cluster_data_prep(csv_file):
@@ -42,47 +43,64 @@ def cluster_data_prep(csv_file):
     return clust_data, data_home, data_away
 
 
-def trend(csv_file, n_clusters=20):
+def trend(csv_file, n_clusters=20, scaled=False):
     '''
     A function to run data_prep, k-means clustering, and tactical identification.
     (Also influences feature selection)
 
     :param csv_file: [type: csv file]
     :param n_clusters: [type: int]
+    :param scaled: default--> False ; normalize data-set using MinMax scaling
     :return: df: [type: pandas dataframe]  tactics pattern cluster dataframe
     '''
     clust_data, _, _ = cluster_data_prep(csv_file)
-    tactics_df = tactical_clust_patterns(clust_data, n_clusters)
+    tactics_df = tactical_clust_patterns(clust_data, n_clusters, scaled)
     return tactics_df
 
 
-def predict(clust_data, n_clusters=20):
+def predict(clust_data, n_clusters=20, scaled=False):
     '''
     A function that returns predictions based on k-means clustering
 
     :param clust_data: [type: pandas dataframe]
     :param n_clusters: [type: int] - number of clusters for clustering
+    :param scaled: default--> False ; normalize data-set using MinMax scaling
     :return: dataframe: [type: pandas dataframe] - tactical patterns of each cluster
     '''
+    if scaled:
+        minmax = MinMaxScaler().fit(clust_data)
+        clust_data_minmax_scaled = minmax.transform(clust_data)
+        clust_data_scaled = pd.DataFrame(clust_data_minmax_scaled, columns=clust_data.columns)
+        clustData = clust_data_scaled
+    else:
+        clustData = clust_data
     kmeans = KMeans(n_clusters, random_state=123)
-    kmeans.fit(clust_data.values)
-    y_pred = kmeans.predict(clust_data.values)
+    kmeans.fit(clustData.values)
+    y_pred = kmeans.predict(clustData.values)
     return y_pred
 
 
-def tactical_clust_patterns(clust_data, n_clusters=20):
+def tactical_clust_patterns(clust_data, n_clusters=20, scaled=False):
     '''
     A function that returns a cluster dataframe showing different tactical similarities in each cluster
     The cluster dataframe is computed using k-means clustering (cluster_centers)
 
     :param clust_data: [type: pandas dataframe]
     :param n_clusters: [type: int] - number of clusters for clustering
+    :param scaled: default--> False ; normalize data-set using MinMax scaling
     :return: dataframe: [type: pandas dataframe] - tactical patterns of each cluster
     '''
+    if scaled:
+        minmax = MinMaxScaler().fit(clust_data)
+        clust_data_minmax_scaled = minmax.transform(clust_data)
+        clust_data_scaled = pd.DataFrame(clust_data_minmax_scaled, columns=clust_data.columns)
+        clustData = clust_data_scaled
+    else:
+        clustData = clust_data
     kmeans = KMeans(n_clusters, random_state=123)
-    kmeans.fit(clust_data.values)
-    clust_kmeans = pd.DataFrame(kmeans.cluster_centers_, columns=clust_data.columns)
-    clust_stats = clust_data.describe(percentiles=[0.1, 0.35, 0.65, 0.9])
+    kmeans.fit(clustData.values)
+    clust_kmeans = pd.DataFrame(kmeans.cluster_centers_, columns=clustData.columns)
+    clust_stats = clustData.describe(percentiles=[0.1, 0.35, 0.65, 0.9])
     clust_kmeans_exp = clust_kmeans.copy()
     for col in clust_kmeans.columns:
         for i, row in enumerate(clust_kmeans[col]):
@@ -124,21 +142,58 @@ def percentiles_clusters(clust_kmeans_exp):
     return clust_very_high, clust_high, clust_med, clust_low, clust_very_low
 
 
-def k_elbow_plot(csv_file):
+def high_percentiles(clust_kmeans_exp):
+    '''
+    A function that breaks the tactics cluster dataframe into 5 different dataframes
+    (Very High, High, Medium, Low, Very Low) and then returns (Very High, High) in a
+    dataframe
+
+    :param clust_kmeans_exp: type: pandas dataframe] - tactical patterns of each cluster
+    :return:
+    '''
+    high_percents = clust_kmeans_exp[(clust_kmeans_exp[clust_kmeans_exp.columns] == 'V. High') |
+                     (clust_kmeans_exp[clust_kmeans_exp.columns] == 'High')].fillna('-')
+    return high_percents
+
+
+def low_percentiles(clust_kmeans_exp):
+    '''
+    A function that breaks the tactics cluster dataframe into 5 different dataframes
+    (Very High, High, Medium, Low, Very Low) and then returns (Very Low, Low) in a
+    dataframe
+
+    :param clust_kmeans_exp: type: pandas dataframe] - tactical patterns of each cluster
+    :return:
+    '''
+    low_percents = clust_kmeans_exp[(clust_kmeans_exp[clust_kmeans_exp.columns] == 'V. Low') |
+                     (clust_kmeans_exp[clust_kmeans_exp.columns] == 'Low')].fillna('-')
+    return low_percents
+
+
+def k_elbow_plot(csv_file, scaled=False):
     '''
     A function that creates a k-elbow plot to help identify optimal k value
+
     :param csv_file: [type: csv]
+    :param scaled: default--> False ; normalize data-set using MinMax scaling
     :return:
     '''
     # k means determine k
     clust_data, _, _ = cluster_data_prep(csv_file)
+    if scaled:
+        minmax = MinMaxScaler().fit(clust_data)
+        clust_data_minmax_scaled = minmax.transform(clust_data)
+        clust_data_scaled = pd.DataFrame(clust_data_minmax_scaled, columns=clust_data.columns)
+        clustData = clust_data_scaled
+    else:
+        clustData = clust_data
     distortions = []
-    cluster_range = range(1,30)
+    cluster_range = range(1, 30)
     for k in cluster_range:
         kmeanModel = KMeans(n_clusters=k)
-        kmeanModel.fit(clust_data)
-        distortions.append(sum(np.min(cdist(clust_data,
-                        kmeanModel.cluster_centers_, 'euclidean'), axis=1))/clust_data.shape[0])
+        kmeanModel.fit(clustData)
+        distortions.append(sum(np.min(cdist(clustData,
+                        kmeanModel.cluster_centers_, 'euclidean'), axis=1))/clustData.shape[0])
     # print(distortions)
 
     # Plot the elbow
@@ -153,6 +208,8 @@ def k_elbow_plot(csv_file):
 
 if __name__ == '__main__':
     csvFILEpath = input("Enter path to file that you wish to pre-process: (should be a .csv file) ")
-    kmeans_tactics_df = trend(csvFILEpath)
+    kmeans_tactics_df = trend(csvFILEpath, 20, True)
+    low_and_very_low = low_percentiles(kmeans_tactics_df)
+    high_and_very_high = high_percentiles(kmeans_tactics_df)
     k_elbow_plot(csvFILEpath)
 
